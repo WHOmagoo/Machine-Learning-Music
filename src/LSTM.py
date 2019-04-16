00b331c9ff3057f1ce7290741ad46f3568f06ff8
gduncan1 <glenn.duncan@wsu.edu>
2019-04-16 11:04:15 -0700

    Some basic code for an LSTM.

---
+++ src/LSTM.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Blocks warning messages
+
+
+if __name__ == "__main__":
+    session = tf.Session()
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+
+    print(x_train)
+    print(x_train[0].shape)
+
+    model = Sequential()
+
+    model.add(LSTM(128, input_shape=(x_train.shape[1:]), activation='relu', return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(128, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(10, activation='softmax'))
+
+    opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
+
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=3, validation_data=(x_test, y_test))
+
